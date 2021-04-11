eac985be3608ba722c4595b47b09258e1bbe2713
Alex Kinley <awkinley@gmail.com>
2021-04-11 15:20:56 -0400

    DBTable prototype

--- src/main/java/edu/wpi/p/database/DBTable.java
+++ src/main/java/edu/wpi/p/database/DBTable.java
@@ -7,9 +7,29 @@ public class DBTable {
     private List<DBColumn> columns;
 
 
-    public DBTable(String name, List<DBColumn> columns) {
-        this.name = name;
-        this.columns = columns;
+//    public DBTable(String name, List<DBColumn> columns) {
+//        this.name = name;
+//        this.columns = columns;
+//    }
+
+    public void updateNode(int nodeID, Node newNode) {
+
+    }
+
+    public void addNode(Node newNode) {
+
+    }
+
+    public void addEdge(Node node1, Node node2) {
+
+    }
+
+    public void removeEdge(Node node1, Node node2) {
+
+    }
+
+    public void removeNode(Node node) {
+
     }
 
 
