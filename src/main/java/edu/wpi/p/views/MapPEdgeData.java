e391be412bc4df1e059d09ba813e29e57de6cc46
Rohan Anand <rohananand@hotmail.com>
2021-04-13 16:15:51 -0400

    Gave edge and node a home button and made the action go to the home page

--- src/main/java/edu/wpi/p/views/MapPEdgeData.java
+++ src/main/java/edu/wpi/p/views/MapPEdgeData.java
@@ -1,8 +1,14 @@
 package edu.wpi.p.views;
 
+import edu.wpi.p.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+
+import java.io.IOException;
 
 public class MapPEdgeData {
 
@@ -14,4 +20,14 @@ public class MapPEdgeData {
     private Button deleteEdgeBtn;
     @FXML
     private TableView edgeDataTableView;
+    @FXML
+    private Button homeButton;
+    public void homeButtonAc(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/p/fxml/HomePage.fxml"));
+            App.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
