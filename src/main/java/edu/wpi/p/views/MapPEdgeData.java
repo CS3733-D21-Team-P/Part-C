f6859c03daa89192cccce4fed03d7198cb8fdf33
David F <davfournet@sbcglobal.net>
2021-04-12 20:59:56 -0400

    added controller classes for MapPEdgeData and MapPNodeData

---
+++ src/main/java/edu/wpi/p/views/MapPEdgeData.java
@@ -0,0 +1,17 @@
+package edu.wpi.p.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class MapPEdgeData {
+
+    @FXML
+    private Button addEdgeBtn;
+    @FXML
+    private Button editEdgeBtn;
+    @FXML
+    private Button deleteEdgeBtn;
+    @FXML
+    private TableView edgeDataTableView;
+}
---
+++ src/main/java/edu/wpi/p/views/MapPNodeData.java
@@ -0,0 +1,17 @@
+package edu.wpi.p.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class MapPNodeData {
+
+    @FXML
+    private Button addNodeBtn;
+    @FXML
+    private Button editNodeBtn;
+    @FXML
+    private Button deleteNodeBtn;
+    @FXML
+    private TableView nodeDataTableView;
+}
