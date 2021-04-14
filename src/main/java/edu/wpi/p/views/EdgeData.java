ca40e88d27a3625f3cdc58c3012bb89a4131406c
Drew <amarnold@wpi.edu>
2021-04-14 13:14:16 -0400

    Added EdgeData class to git

---
+++ src/main/java/edu/wpi/p/views/EdgeData.java
@@ -0,0 +1,31 @@
+package edu.wpi.p.views;
+
+import javafx.beans.property.SimpleStringProperty;
+public class EdgeData {
+    SimpleStringProperty edgeID;
+    SimpleStringProperty startNode;
+    SimpleStringProperty endNode;
+    EdgeData(String edgeID, String startNode, String endNode) {
+        this.edgeID = new SimpleStringProperty(edgeID);
+        this.startNode = new SimpleStringProperty(startNode);
+        this.endNode = new SimpleStringProperty(endNode);
+    }
+    public String getEdgeID(){
+        return edgeID.get();
+    }
+    public void setEdgeID(String fname){
+        edgeID.set(fname);
+    }
+    public String getStartNode(){
+        return startNode.get();
+    }
+    public void setEndNode(String fpath){
+        startNode.set(fpath);
+    }
+    public String getSize(){
+        return endNode.get();
+    }
+    public void setSize(String fsize){
+        endNode.set(fsize);
+    }
+}
