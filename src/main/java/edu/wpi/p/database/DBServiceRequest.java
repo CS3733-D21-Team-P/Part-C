a670b1757dc0f9a27eb941174980fa4f62dba35e
Yoko-Guang-Yang <79557530+iwantaloli@users.noreply.github.com>
2021-04-18 14:09:31 -0400

    Try to write the DBServiceRequest class.

--- src/main/java/edu/wpi/p/database/DBServiceRequest.java
+++ src/main/java/edu/wpi/p/database/DBServiceRequest.java
@@ -1,10 +1,50 @@
 package edu.wpi.p.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBServiceRequest {
 
+    private String serviceRequestTable = "SRT";
+    private List<DBColumn> serviceRequestColumn;
+
+    public void serviceRequestTable(List<DBColumn> serviceRequestColumn){
+        this.serviceRequestColumn = serviceRequestColumn;
+
+    }
 
 
+    private void init() {
+        List<DBColumn> columns = new ArrayList<>();
+        columns.add(new DBColumn("name", "varchar(256)", ""));
+        columns.add(new DBColumn("id", "varchar(256)", ""));
+        columns.add(new DBColumn("location", "varchar(256)", ""));
+        columns.add(new DBColumn("whatever else", "varchar(256)", ""));
+    }
 
+    public void DBServiceRequestTable(List<DBColumn> nodeColumns, List<DBColumn> edgeColumns) {
+        this.serviceRequestColumn = serviceRequestColumn;
+
+
+        this.createTables(false);
+
+    }
+    public void clearServiceRequest() {
+        DatabaseInterface.executeUpdate("DROP TABLE " + serviceRequestTable);
+    }
+
+    private void createTables(boolean doClear) {
+        if (doClear) {
+            clearServiceRequest();
+        }
+        DatabaseInterface.createTableIfNotExists(serviceRequestTable, this.serviceRequestColumn);
+
+    }
+
+    public boolean createNodeTable(List<DBColumn> columns) {
+        this.serviceRequestColumn = columns;
+        return DatabaseInterface.createTableIfNotExists(serviceRequestTable, columns);
+    }
 
 
 
