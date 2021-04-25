81e88f65659aca5c4faf2249fe99801b65f12c4b
Rohan Anand <rohananand@hotmail.com>
2021-04-25 19:20:54 -0400

    Made home page update and connected buttons to them

--- src/main/java/edu/wpi/p/views/LoginPage.java
+++ src/main/java/edu/wpi/p/views/LoginPage.java
@@ -1,26 +1,10 @@
 package edu.wpi.p.views;
 import com.jfoenix.controls.*;
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import edu.wpi.p.App;
-import edu.wpi.p.database.DBServiceRequest;
-import edu.wpi.p.database.DBTable;
-import edu.wpi.p.database.Edge;
-import edu.wpi.p.database.ServiceRequest;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableCell;
-import javafx.scene.control.TreeTableColumn;
-import javafx.util.Callback;
-import com.jfoenix.controls.JFXTreeTableView;
 
 import java.io.IOException;
 
