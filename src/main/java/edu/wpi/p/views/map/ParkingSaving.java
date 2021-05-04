f59c80a0428631a0b41676a490669076d8c9f83d
Drew <amarnold@wpi.edu>
2021-05-04 16:55:33 -0400

    Init now checks which users have saved parking spots and highlight them

--- src/main/java/edu/wpi/p/views/map/ParkingSaving.java
+++ src/main/java/edu/wpi/p/views/map/ParkingSaving.java
@@ -3,12 +3,17 @@ package edu.wpi.p.views.map;
 import com.jfoenix.controls.*;
 import edu.wpi.p.AStar.Node;
 import edu.wpi.p.AStar.NodeButton;
+import edu.wpi.p.AStar.NodeGraph;
 import edu.wpi.p.database.DBUser;
 import edu.wpi.p.database.UserFromDB;
+import org.sqlite.core.DB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingSaving {
-    JFXButton saveParking;
     private final DBUser dbuser = new DBUser();
+
 
     public void saveParkingAc(NodeButton nb) {
         UserFromDB user = new UserFromDB("tempUser", "tempUser");
